41611d5148c35c2985145aee7628d79c9c427fbe
Adarsh <adarshuppuleti@gmail.com>
2024-07-30 22:53:27 +0530

    30-07-24

---
+++ Practice_30-07-24.py
@@ -0,0 +1,100 @@
+
+# Write a python program to  add a key to a dictionary ?
+# m -1
+d = {'comany':"skywaves",'locality':'Kondapur'}
+d['name']='Adarsh'
+d['age']='24'
+d['role']='Python developer'
+print(d)
+
+# m - 2
+d = {'comany':"skywaves",'locality':'Kondapur'}
+d1 ={'name':'Adarsh'}
+d.update(d1)
+print(d)
+
+# Write a python program to check weather the given value is present in the dictionary or not ?
+
+i = eval(input("enter the value :"))
+dic = {'one':1,'two':2,'name':"adarsh",'age':24,'key':'Value'}
+key = dic.values()
+if i in key:
+    print("Yes exist")
+else:
+    print("Not exist")
+
+# Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are the square of the keys.
+#Sample Dictionary
+#{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}"
+dic = {}
+for i in range(1,16):
+    key = i
+    value = i*i
+    dic[key]=value
+print(dic)
+
+# Write a python program to create a dictionary from the string ?
+
+dic = {}
+st = "Adarsh"
+key = ''
+value = ''
+for i in st:
+    if i.islower():
+        key = i
+        value = 'it is lower'
+        dic[key]=value
+    else:
+        key = i
+        value = 'it is upper'
+        dic[key]=value
+print(dic)
+
+# Write a python program to merge two python dictionaries ?
+d1 = {'name':'adarsh','role':'python developer'}
+d2 = {'age':24,'company':'skywaves'}
+d1.update(d2)
+print(d1)
+
+# Write  a python program to sort dictionary by keys or values ?
+# m -1
+d1 = {'b':'one','a':'two'}
+res =sorted(d1.items())
+print(res)
+# m - 2
+d1 = {'b':'one','a':'two'}
+res =sorted(d1.keys())
+print(res)
+# m - 3
+d1 = {'b':2,'a':1}
+res =sorted(d1.values())
+print(res)
+
+#Write a Python program to create a dictionary from a string.  Note: Track the count of the letters from the string.
+#Sample string : 'w3resource'
+#Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}"
+
+st = input("Enter a string :")
+count = 0
+di = {}
+if st.isalpha:
+    for i in st :
+        count = st.count(i)
+        di[i]=count
+else:
+    print("Enter only alphabets")
+print(di)
+
+
+# Write a python program to combine two dictionaries by adding values of common keys ?
+
+d1 = {'a':100,'b':200,'c':300,'d':400}
+d2 = {'c':1000,'d':2000}
+new = {}
+key1 = d1.keys()
+key2 = d2.keys()
+for i in key1:
+    if i in key2:
+        sum = d1[i]+d2[i]
+        new[i]=sum
+print(new)
---
+++ Task_30-07-24.py
@@ -0,0 +1,44 @@
+"""
+Write a python program to collect data from a user and store it in a dictionary, 
+you can use Python's built-in functions like input() to get user input and then organize that input into a dictionary. 
+Note :- Data is employee name, domain, performance(like credits = 3/5).
+"""
+print("Enter The employee Data here Below")
+num = int(input("Enter The Number Of employees :"))
+dic = {}
+for i in range(num):
+    name = input("Enter the Employee name :")
+    Domain = input('Enter the Domain :')
+    Performance = int(input("Enter The credits for employee out of 5 :"))
+    if name!='' and Domain!='' and Performance < 5 :
+        dic['Name']=name
+        dic['Domain']=Domain
+        dic['Credits Out of 5']=Performance
+    else:
+        print("Enter Proper data")
+print(dic)
+
+
+# packing and Unpacking of tuple
+a = (10,20,30)
+b,c,d = a
+print(b)
+print(c)
+print(d)
+
+dta = ("Adarsh",24,"Python Developer")
+name,age,role=dta
+print(name)
+print(age)
+print(role)
+
+# dictionary from user
+ele = input("Enter the number of elements  :")
+dic = {}
+if ele.isdigit() and len(ele)==1:
+    for i in range(int(ele)):
+        key = input("Enter the key :")
+        value = input("Enter The Value :")
+        dic[key]=value
+print(dic)
+        
